28b6699b1906f84da93d4dc9ca5b323539fcfcd7
Dustin Norlander <dustinn@gmail.com>
2012-08-30 16:55:45 -0700

    initial commit, moved from cheshire project

---
+++ src/main/LogbackConfigure.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+
+
+/**
+ * @author Dustin Norlander
+ * @created Mar 12, 2012
+ * 
+ */
+public class LogbackConfigure {
+	
+	public static void configLogback(String configFilename) {
+	    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+	       try {
+	           JoranConfigurator configurator = new JoranConfigurator();
+	           configurator.setContext(lc);
+	           lc.reset();
+	           configurator.doConfigure(configFilename);
+	       } catch (JoranException je) {
+	           je.printStackTrace();
+	       }
+	}
+	
+}
